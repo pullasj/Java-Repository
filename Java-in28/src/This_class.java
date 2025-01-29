f4400ed166c4b18f554acc50b42dc6244df5f155
Surya Sarath Pulla <155488064+pullasj@users.noreply.github.com>
2025-01-29 18:27:14 -0500

    updated

---
+++ Java-in28/src/This_class.java
@@ -0,0 +1,15 @@
+public class This_class {
+    private String name;
+    private int age;
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setAge(int age1){
+        this.age= age1;
+    }
+    public void printDetails(){
+        System.out.println("Name of the person is :"+name+ " age is "+age);
+    }
+}
---
+++ Java-in28/src/This_class_Main.java
@@ -0,0 +1,10 @@
+public class This_class_Main {
+    public static void main(String[] args){
+
+        This_class person1 = new This_class();
+        person1.setName("Koya Koya");
+        person1.setAge(54);
+
+        person1.printDetails();
+    }
+}
